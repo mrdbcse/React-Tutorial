6314e71de685963033379317911bde86e5a869c9
debjyoti2013 <debjyoti@b4b.ai>
2021-10-25 18:09:08 +0530

    Init

---
+++ src/Components/User.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class User extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello from User Class Component</h1>
+      </div>
+    );
+  }
+}
